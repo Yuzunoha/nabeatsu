b8251585a65fac54d8b986f1bc12aef40f8ec3a7
Yuzunoha <nurugamer2@gmail.com>
2020-02-06 22:54:47 +0900

    できたっぽい

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const isContains3 = num => {
+  while (true) {
+    if (num % 10 === 3) {
+      return true;
+    }
+    num -= num % 10;
+    num /= 10;
+    if (num === 0) {
+      break;
+    }
+  }
+  return false;
+};
+
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  if (i % 3 === 0) {
+    // 3の倍数
+    console.log(i + '!');
+  } else if (isContains3(i)) {
+    // 3を含む
+    console.log(i + '!');
+  } else {
+    console.log(i);
+  }
 }
